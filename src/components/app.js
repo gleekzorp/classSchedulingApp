4b18dcce7d9f025b435684422a00fd0d1b857bde
gleekzorp <gleek.dan@gmail.com>
2019-06-03 14:11:37 -0600

    Setup course library component

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import CourseLibrary from './courselibrary';
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
         <h1>Course Library</h1>
+        <CourseLibrary />
       </div>
     );
   }
